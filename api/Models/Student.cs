e0d09ee97c37c5504ef9c85d72fe6c94060517f4
Jpdl925 <jtpg925@gmail.com>
2024-04-25 19:54:03 -0700

    fixed bracket position

--- api/Models/Student.cs
+++ api/Models/Student.cs
@@ -9,10 +9,11 @@ namespace api.Models
     public class Student
     {
         public int Id { get; set; }
-    }
+    
 
     public string? Name { get; set; }
     public string? Address { get; set; }
     public string? PhoneNumber { get; set; }
     public string? Email { get; set; }
+    }
 }
